ab66fc945faa7aa72074966f652239304cd96eb6
Dom-Henrique <joaohenriqueds24@gmail.com>
2026-03-27 07:28:07 -0300

    Delete button function

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const tasks = document.querySelector(".tasks");
-let addTask = document.querySelector("#taskBt");
+const addTask = document.querySelector("#taskBt");
+const deleteTask = document.querySelector(".deleteTasks");
 // funcao que recebe os valores dos campos e adiciona
 
 addTask.addEventListener("click", function regTask() {
@@ -8,7 +9,7 @@ addTask.addEventListener("click", function regTask() {
     let taskDate = document.querySelector("#taskDate").value;
 
     // cria os elementos
-    if (taskName != "" && taskDate != "") {
+    if (taskName != "" && taskDate != "" && taskDesc != "") {
         let divMainly = document.createElement("div");
         let div = document.createElement("div");
         div.className = "header";
@@ -23,5 +24,11 @@ addTask.addEventListener("click", function regTask() {
         div.append(h3, h4);
         divMainly.append(div, p);
         tasks.appendChild(divMainly);
+    }
+})
+
+deleteTask.addEventListener("click", () => {
+    while (tasks.firstChild){
+        tasks.removeChild(tasks.firstChild);
     }
 })
